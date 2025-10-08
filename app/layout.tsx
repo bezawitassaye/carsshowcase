e14b9db2f7c43c4b3d18eea799ba37bfdc7b9b8d
bezawitassaye <beza1221love@gmail.com>
2025-10-09 02:00:45 +0300

    create navbar and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 
 
@@ -17,8 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className="relative">
-          
+        <Navbar/>  
         {children}
+        <Footer/>
       </body>
     </html>
   );
