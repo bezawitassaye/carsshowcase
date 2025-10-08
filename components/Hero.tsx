46be4b56b3654615b06559005ff06996b6da682f
bezawitassaye <beza1221love@gmail.com>
2025-10-09 01:59:40 +0300

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,19 @@ const Hero = () => {
         />
         </div> 
 
-        <div>
+        <div className='hero__image-container'>
+          <div className='hero__image'>
+            <Image 
+              src="/hero.png"
+              alt="hero"
+              fill
+              className='object-contain'
+            />
+          </div>
+
+          <div className='hero__image-overlay'>
+
+          </div>
           
         </div>
       
