b004e09df69532150731e46f9a396d40ac02619e
bezawitassaye <beza1221love@gmail.com>
2025-10-09 22:11:53 +0300

    update

--- utils/index.ts
+++ utils/index.ts
@@ -1,22 +1,3 @@
-import { asyncWrapProviders } from "async_hooks";
-
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'cc8f2ab261msh1eb5b4e18a9f0eap16ecb2jsn55494e8c289d',
-		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-
 
 export async function fetchCars() {
   const headers = {
@@ -27,6 +8,9 @@ export async function fetchCars() {
   const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
     headers: headers,
   });
+   
+  const result = await response.json();
 
+  return result;
 }
    
