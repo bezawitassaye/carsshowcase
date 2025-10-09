dac2b3698502085dd8ffb360c51a508af649d87b
bezawitassaye <beza1221love@gmail.com>
2025-10-09 21:46:34 +0300

    update

--- components/Searchmanufacturer.tsx
+++ components/Searchmanufacturer.tsx
@@ -18,7 +18,7 @@ const Searchmanufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
           <Combobox.Button className='absolute top-[14px]'>
             <Image src="/car-logo.svg" width={20} height={20} className='ml-4' alt='car logo' />
@@ -47,7 +47,19 @@ const Searchmanufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                     }`
                   }
                 >
-                  {item}
+                  {({ selected, active }) => (
+                      <>
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                          {item}
+                        </span>
+
+                        {/* Show an active blue background color if the option is selected */}
+                        {selected ? (
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
                 </Combobox.Option>
               ))}
 
