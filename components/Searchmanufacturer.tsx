80ef705687c51198b8ce35bf909b3a6b18cddba0
bezawitassaye <beza1221love@gmail.com>
2025-10-09 20:23:33 +0300

    update

--- components/Searchmanufacturer.tsx
+++ components/Searchmanufacturer.tsx
@@ -1,8 +1,17 @@
+"use client"
+
+import { Combobox,Transition } from '@headlessui/react'
+import { SearchManufacturerProps } from '@/types'
 import React from 'react'
 
-const Searchmanufacturer = () => {
+const Searchmanufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
   return (
-    <div>
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className='relative w-full'> 
+
+        </div>
+      </Combobox>
       
     </div>
   )
