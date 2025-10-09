d75921b82d4148519729b764e2eb029d39ecd01f
bezawitassaye <beza1221love@gmail.com>
2025-10-09 19:48:08 +0300

    imports

---
+++ components/index.ts
@@ -0,0 +1,14 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+
+export {
+    Hero,
+    Navbar,
+    CustomButton,
+    SearchBar,
+    CustomFilter
+
+}
