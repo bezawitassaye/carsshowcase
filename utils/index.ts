0bd6daf2b9d68b30043ff024c9a9641210bb04ea
bezawitassaye <beza1221love@gmail.com>
2025-10-09 22:10:08 +0300

    update

--- utils/index.ts
+++ utils/index.ts
@@ -19,10 +19,14 @@ try {
 
 
 export async function fetchCars() {
-  const response = await fetch( 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla')
-  
-  const headers={
-      'x-rapidapi-key': 'cc8f2ab261msh1eb5b4e18a9f0eap16ecb2jsn55494e8c289d',
-      'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-    }
+  const headers = {
+    'x-rapidapi-key': 'cc8f2ab261msh1eb5b4e18a9f0eap16ecb2jsn55494e8c289d',
+    'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
   };
+
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    headers: headers,
+  });
+
+}
+   
