ad20a0949917aea4cbf01b9e709f8f463d81a8e0
bezawitassaye <beza1221love@gmail.com>
2025-10-09 21:17:16 +0300

    update

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,14 @@
+"use client"
 import React from 'react'
+import Searchmanufacturer from './Searchmanufacturer'
 
 const SearchBar = () => {
   return (
     <div>
-      
+       <Searchmanufacturer
+        manufacturer=''
+        setManufacturer={()=>{}}
+        />
     </div>
   )
 }
