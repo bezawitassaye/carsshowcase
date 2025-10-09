9295245b5385d27fce99ceedb15b5c1c99d12c3f
bezawitassaye <beza1221love@gmail.com>
2025-10-09 23:00:19 +0300

    create card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CustomFilter, SearchBar } from "@/components";
+import CarCard from "@/components/CarCard";
 import Hero from "@/components/Hero";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -6,6 +7,8 @@ import Image from "next/image";
 export default async function Home() {
  
   const allCars = await fetchCars()
+
+  const isdataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -24,6 +27,21 @@ export default async function Home() {
         <div className="home__filter-container">
           <CustomFilter/>
         </div>
+
+        {!isdataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car)=>(
+                <CarCard car={car}/>
+              ))}
+            </div>
+          </section>
+        ):(
+          <div className="home__error-container">
+            <h1 className="text-black text-xl font-bold">Oops, no results</h1>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
         
       </div>
     </main>
